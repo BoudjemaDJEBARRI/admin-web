b756facba87e4feeeebdba2c05bc46a90f8eb31f
Boudjema DJEBARRI <boudjema.djebari@gmail.com>
2023-01-20 20:49:55 +0100

    Initial commit

---
+++ src/main/java/com/scoolcool/data/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.scoolcool.data.repository;
+
+import com.scoolcool.data.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+}
---
+++ src/main/java/com/scoolcool/data/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.scoolcool.data.repository;
+
+import com.scoolcool.data.model.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long> {
+    Iterable<Order> findAllByCustomerId(long customerId);
+}
